ab13c2b6b54c161e44a1a0651969d679e3bc410a
Seth McCullough <sethmckilladev@gmail.com>
2020-12-08 08:35:57 -0500

    Add search widget

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a front end javascript framework",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite among JS engineers",
-  },
-  {
-    title: "How do you use React?",
-    content: "React is built upon components and state",
-  },
-];
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front end javascript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite among JS engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "React is built upon components and state",
+//   },
+// ];
 
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
