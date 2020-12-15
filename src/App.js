e485a143c5cbac91f65b472e9f436056b5e956e7
Seth McCullough <sethmckilladev@gmail.com>
2020-12-15 08:02:55 -0500

    Finish wiki search and add accordion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 // const items = [
 //   {
@@ -18,12 +19,34 @@ import Search from "./components/Search";
 // ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
-    <div>
+    <div className="ui container">
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
 
 export default App;
