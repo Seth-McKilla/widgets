b61ce40ddc932ea82a3d094e0393d0cc477db9b0
Seth McCullough <sethmckilladev@gmail.com>
2020-12-18 07:47:15 -0500

    Add Navigation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
 import React, { useState } from "react";
-// import Accordion from "./components/Accordion";
-// import Search from "./components/Search";
-// import Dropdown from "./components/Dropdown";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
-// const items = [
-//   {
-//     title: "What is React?",
-//     content: "React is a front end javascript framework",
-//   },
-//   {
-//     title: "Why use React?",
-//     content: "React is a favorite among JS engineers",
-//   },
-//   {
-//     title: "How do you use React?",
-//     content: "React is built upon components and state",
-//   },
-// ];
-
-const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
-  return (
-    <div className="ui container">
-      {/* <Accordion items={items} /> */}
-      {/* <Search /> */}
-      {/* <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        /> */}
-      <Translate />
-    </div>
-  );
-};
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite among JS engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "React is built upon components and state",
+  },
+];
 
 const options = [
   {
@@ -51,5 +35,32 @@ const options = [
     value: "blue",
   },
 ];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className="ui container">
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
+};
 
 export default App;
